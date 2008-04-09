e668678fd5626fe58fd63f59a46794fbe95d9bc2
diergo <diergo@7ceceba0-f547-0410-9250-cdd8bca831fd>
2008-04-09 15:03:55 +0000

    mocks

--- trunk/src/diergo/array/ArrayReaderIteratorTest.java
+++ trunk/src/diergo/array/ArrayReaderIteratorTest.java
@@ -4,63 +4,39 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.EOFException;
-import java.io.IOException;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import org.junit.Test;
+
+import diergo.array.test.MockArrayReader;
 
 public class ArrayReaderIteratorTest
 {
 	@Test
 	public void emptyReaderResultsInEmptyIterator()
 	{
-		ArrayReader<String> in = new TestReader(new String[0][]);
+		ArrayReader<String> in = new MockArrayReader<String>(Arrays.asList(new String[0][]));
 		assertFalse(new ArrayReaderIterator<String>(in).hasNext());
 	}
 
 	@Test(expected=NoSuchElementException.class)
 	public void emptyReaderThrowsExceptionOnNext()
 	{
-		ArrayReader<String> in = new TestReader(new String[0][]);
+		ArrayReader<String> in = new MockArrayReader<String>(Arrays.asList(new String[0][]));
 		new ArrayReaderIterator<String>(in).next();
 	}
 	
 	@Test
 	public void linesFromReaderAreReturned()
 	{
-		ArrayReader<String> in = new TestReader(new String[][] {{"1a", "1b"}, {"2a", "2b"}});
+		ArrayReader<String> in = new MockArrayReader<String>(Arrays.asList(new String[][] {{"1a", "1b"}, {"2a", "2b"}}));
 		Iterator<String[]> i = new ArrayReaderIterator<String>(in);
 		assertTrue(i.hasNext());
 		assertArrayEquals(new String[] {"1a", "1b"}, i.next());
 		assertTrue(i.hasNext());
 		assertArrayEquals(new String[] {"2a", "2b"}, i.next());
 		assertFalse(i.hasNext());
-	}
-
-	private static class TestReader implements ArrayReader<String>
-	{
-		private final String[][] _lines;
-		private int _index;
-		
-		public TestReader(String[][] lines)
-		{
-			_lines = lines;
-			_index = 0;
-		}
-
-		public void close()
-		{
-		}
-
-		public String[] read() throws IOException
-		{
-			if (_index < _lines.length) {
-				return _lines[_index++];
-			}
-			throw new EOFException();
-		}
-		
 	}
 }
