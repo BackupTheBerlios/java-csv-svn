856a1882b8d73faaecf1fd4165a56393588f0d45
diergo <diergo@7ceceba0-f547-0410-9250-cdd8bca831fd>
2008-04-11 10:49:11 +0000

    simplified usage by new StringArrayCutter

---
+++ trunk/src/diergo/csv/StringArrayCutter.java
@@ -0,0 +1,34 @@
+package diergo.csv;
+
+import diergo.array.ArrayCutter;
+import diergo.array.ArrayReader;
+import diergo.array.ArrayWriter;
+
+/**
+ * A utility class to cut selected fields of string arrays.
+ * Cutting string array readers and writers are offered.
+ */
+public class StringArrayCutter extends ArrayCutter<String>
+{
+	/**
+	 * Creates a new string array reader passing the selected fields only.
+	 */
+	public static ArrayReader<String> cut(ArrayReader<String> in, int[] fields)
+	{
+		return ArrayCutter.cut(in, fields);
+	}
+	
+	/**
+	 * Creates a new string array writer passing the selected fields only.
+	 */
+	public static ArrayWriter<String> cut(ArrayWriter<String> in, int[] fields)
+	{
+		return ArrayCutter.cut(in, fields);
+	}
+
+	public StringArrayCutter(int[] fields)
+	{
+		super(fields);
+	}
+
+}
