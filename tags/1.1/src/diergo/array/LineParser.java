eea48837761c31ebceea7889488b96923eb5339e
diergo <diergo@7ceceba0-f547-0410-9250-cdd8bca831fd>
2010-02-15 12:42:13 +0000

    wrong revision

--- tags/1.1/src/diergo/array/LineParser.java
+++
@@ -1,8 +0,0 @@
-package diergo.array;
-
-public interface LineParser<E>
-{
-    public E[] parseLine(String line);
-
-    public String generateLine(E... values);
-}
--- tags/1.1/src/diergo/array/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package provides array utilities.
- */
-package diergo.array;
